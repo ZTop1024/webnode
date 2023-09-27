a75ee2d3701d615be66b2b1906a5bd6332d6d9c9
zhoutuo <zhoutuo@wxchina.com>
2023-09-27 21:57:39 +0800

    更新

---
+++ node-picture-transform/src/main/java/com/webnode/Main.java
@@ -0,0 +1,15 @@
+package com.webnode;
+
+import cn.hutool.core.img.ImgUtil;
+
+/**
+ * @author <a href="zhoutuo@wxchina.com">zhoutuo</a>
+ * @date 2023/9/27
+ **/
+public class Main {
+
+    public static void main(String[] args) {
+        ImgUtil.toBase64()
+    }
+
+}
