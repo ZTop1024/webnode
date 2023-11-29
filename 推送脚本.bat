e7a6c9c4190461f812668ee4d8b0eccaf087293e
ZTop <ztop.zhou@foxmail.com>
2023-11-29 23:01:22 +0800

    echo 2023/11/29 23:01:22.52-更新脚本

--- 推送脚本.bat
+++ 推送脚本.bat
@@ -8,6 +8,12 @@ echo + ------------- 开始推送gitee仓库 ------------- +
 git add *
 git commit -m "echo %date:~3,10% %time%-%reason%"
 git push gitee master
+if %errorlevel% equ 0 (
+    echo + ------------- 推送gitee仓库成功 ------------ +
+) else (
+    echo + ------------- 推送gitee仓库失败 ------------ +
+)
+
 echo + ------------- 完成推送gitee仓库 ------------- +
 
 echo ++++++++++++++++++++++++++++++++++++++++++++++++
@@ -15,7 +21,7 @@ echo ++++++++++++++++++++++++++++++++++++++++++++++++
 echo + ------------- 开始推送github仓库 ------------ +
 git push github master
 if %errorlevel% equ 0 (
-    echo + ------------- 完成推送github仓库 ------------ +
+    echo + ------------- 推送github仓库成功 ------------ +
 ) else (
     echo + ------------- 推送github仓库失败 ------------ +
 )
