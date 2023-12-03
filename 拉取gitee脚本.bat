8bfd6e4392beed989a71ea8f38f1070a01f1a972
ZTop <ztop.zhou@foxmail.com>
2023-12-03 11:14:34 +0800

    2023/12/03 11:14-更新脚本

--- 拉取gitee脚本.bat
+++ 拉取gitee脚本.bat
@@ -1,11 +1,12 @@
 @echo
 chcp 65001
 
+echo + ------------- 开始从gitee仓库拉取最新代码 ------------- +
 git pull gitee master
 if %errorlevel% equ 0 (
-    echo + ------------- 拉取gitee仓库成功 ------------ +
+    echo + ------------- 拉取gitee仓库最新代码成功 ------------ +
 ) else (
-    echo + ------------- 拉取gitee仓库失败 ------------ +
+    echo + ------------- 拉取gitee仓库最新代码失败 ------------ +
 )
 
 pause
