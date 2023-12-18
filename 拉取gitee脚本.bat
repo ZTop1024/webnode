b154d46f6ac0ca68654b861c7734e210a9134464
ZTop <ztop.zhou@foxmail.com>
2023-12-18 22:24:56 +0800

    2023/12/18 22:24 - 同步笔记

--- 拉取gitee脚本.bat
+++ 拉取gitee脚本.bat
@@ -3,6 +3,7 @@ chcp 65001
 
 echo + ------------- 开始从gitee仓库拉取最新代码 ------------- +
 git pull gitee master
+
 if %errorlevel% equ 0 (
     echo + ------------- 拉取gitee仓库最新代码成功 ------------ +
 ) else (
